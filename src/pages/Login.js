d4877d4006f9e055415e2495c74eec0c92f8f719
Seddik Ouertani <mohamedseddikouertani@gmail.com>
2021-06-16 00:57:04 +0100

    inital commit

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+
+function Login() {
+  return (
+    <div className="Login">
+      <header className="Login-Header">
+        <p>
+          My name is med Seddik
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Login;
